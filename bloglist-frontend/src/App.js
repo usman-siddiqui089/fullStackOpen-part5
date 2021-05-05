39c42ac149d278797da9593a04b2a3ffa071145c
UsmanSiddiqui <usmansiddiqui@A006-427.local>
2021-05-05 15:10:00 +0500

    added bloglist frontend step2

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -15,6 +15,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem('loggedInUser')
+    if(loggedInUserJSON){
+      const user = JSON.parse(loggedInUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const loginForm = () => (
     <>
       <h2>Login to Application</h2>
@@ -46,7 +54,10 @@ const App = () => {
     <>
       <div>
         <h2>blogs</h2>
-        <p>{user.username} is logged in</p>
+        <p>
+          {user.username} is logged in
+          <button onClick={handleLogout}>Logout</button>
+        </p>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
@@ -60,12 +71,18 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem('loggedInUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (error) {
       alert('Error! Wrong Credentials')
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
   }
 
   if(user === null){
